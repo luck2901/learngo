880673cb1147e8c57b0c8607b790cd7359169668
한상현 <luck2901@naver.com>
2021-07-19 18:03:58 +0900

    7/19

--- myapp/app.go
+++ myapp/app.go
@@ -14,6 +14,10 @@ type User struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
 type fooHandler struct{} //instance를 만들고
 
 //interface사용
@@ -47,9 +51,7 @@ func barHandler(w http.ResponseWriter, r *http.Request) {
 func NewHttpHandler() http.Handler {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello World")
-	})
+	mux.HandleFunc("/", indexHandler)
 
 	mux.HandleFunc("/bar", barHandler)
 
---
+++ myapp/app_test.go
@@ -0,0 +1,62 @@
+package myapp
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIndexPathHandler(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	// if res.Code != http.StatusOK {
+	// 	t.Fatal("Failed!! ", res.Code)
+	// }
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World", string(data)) //data는 byte 타입이므로 string으로
+
+}
+
+func TestBarPathHandler_WithoutName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello World!", string(data)) //data는 byte 타입이므로 string으로
+
+}
+
+func TestBarPathHandler_WithName(t *testing.T) {
+	assert := assert.New(t)
+
+	res := httptest.NewRecorder()
+	req := httptest.NewRequest("GET", "/bar?name=sang", nil)
+
+	mux := NewHttpHandler()
+	mux.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusOK, res.Code)
+
+	data, _ := ioutil.ReadAll(res.Body)
+	assert.Equal("Hello sang!", string(data)) //data는 byte 타입이므로 string으로
+
+}
