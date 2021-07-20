0868c8a130cb727f8036246c54b9cd3279271bd9
한상현 <luck2901@naver.com>
2021-07-20 19:05:00 +0900

    7/20

--- Part3/myapp/app.go
+++ Part3/myapp/app.go
@@ -1,11 +1,25 @@
 package myapp
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/gorilla/mux"
 )
+
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+var userMap map[int]*User
+var lastID int
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Hello World")
@@ -15,20 +29,62 @@ func usersHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "Get UserInfo by /users/{id}")
 }
 
-func getUserInfo89Handler(w http.ResponseWriter, r *http.Request) {
+func getUserInfoHandler(w http.ResponseWriter, r *http.Request) {
+	// vars := mux.Vars(r) //request 넣어주면 알아서 id 파싱
+	// fmt.Fprint(w, "User Id:", vars["id"])
 
-	vars := mux.Vars(r) //request 넣어주면 알아서 id 파싱
-	fmt.Fprint(w, "User Id:", vars["id"])
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+	user, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User Id:", id)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
+func createrUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	//Created User
+	lastID++
+	user.ID = lastID
+	user.CreatedAt = time.Now()
+	userMap[user.ID] = user
+
+	w.Header().Add("Conent-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
 }
 
 //NewHandler make a new myapp handler
 func NewHandler() http.Handler {
+	userMap = make(map[int]*User)
+	lastID = 0
 	mux := mux.NewRouter() //대용
 	//mux := http.NewServeMux()
 
 	mux.HandleFunc("/", indexHandler)
-	mux.HandleFunc("/users", usersHandler)
-	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfo89Handler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createrUserHandler).Methods("POST")
+	mux.HandleFunc("/users/{id:[0-9]+}", getUserInfoHandler)
 
 	return mux
 }
--- Part3/myapp/app_test.go
+++ Part3/myapp/app_test.go
@@ -1,9 +1,12 @@
 package myapp
 
 import (
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -51,5 +54,35 @@ func TestGetUserInfo(t *testing.T) {
 	assert.Equal(http.StatusOK, resp.StatusCode)
 
 	data, _ := ioutil.ReadAll(resp.Body)
-	assert.Contains(string(data), "User Id:89")
+	assert.Contains(string(data), "No User Id:89")
+}
+
+func TestCreateUser(t *testing.T) {
+	assert := assert.New(t)
+
+	ts := httptest.NewServer(NewHandler())
+	defer ts.Close()
+
+	resp, err := http.Post(ts.URL+"/users", "application/json",
+		strings.NewReader(`{"first_name":"sang", "last_name":"han", "email":"luck2901@naver.com"}`))
+
+	assert.NoError(err)
+	assert.Equal(http.StatusCreated, resp.StatusCode)
+
+	user := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user)
+
+	assert.NoError(err)
+	assert.NotEqual(0, user.ID)
+
+	id := user.ID
+	resp, err = http.Get(ts.URL + "/users/" + strconv.Itoa(id))
+	assert.NoError(err)
+	assert.Equal(http.StatusOK, resp.StatusCode)
+
+	user2 := new(User)
+	err = json.NewDecoder(resp.Body).Decode(user2)
+	assert.NoError(err)
+	assert.Equal(user.ID, user2.ID)
+	assert.Equal(user.FirstName, user2.FirstName)
 }
