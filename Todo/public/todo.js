cce35f66ca250e8738a311e7f82c4c53a5ce2737
한상현 <luck2901@naver.com>
2021-07-29 14:24:18 +0900

    7/29

--- Todo/public/todo.js
+++ Todo/public/todo.js
@@ -9,16 +9,17 @@ $(function() {
         var item = $(this).prevAll('.todo-list-input').val();
 
         if (item) {
-            todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' />" + item + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
+            $.post("/todos", { name: item }, addItem)
+            // todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' />" + item + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
             todoListInput.val("");
         }
     });
 
     var addItem = function(item) {
         if (item.completed) {
-            todoListItem.append("<li class='completed'><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' checked='checked' />" + item.name + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
+            todoListItem.append("<li class='completed'" + " id='" + item.id + "'><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' checked='checked' />" + item.name + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
         } else {
-            todoListItem.append("<li><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' />" + item.name + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
+            todoListItem.append("<li " + " id='" + item.id + "'><div class='form-check'><label class='form-check-label'><input class='checkbox' type='checkbox' />" + item.name + "<i class='input-helper'></i></label></div><i class='remove mdi mdi-close-circle-outline'></i></li>");
         }
     };
 
@@ -28,18 +29,38 @@ $(function() {
         });
     });
 
-    todoListItem.on('change', '.checkbox', function() {
+    todoListItem.on('change', '.checkbox', function () {
+        var id = $(this).closest("li").attr('id');
+        var $self = $(this);
+        var complete = true;
         if ($(this).attr('checked')) {
-            $(this).removeAttr('checked');
-        } else {
-            $(this).attr('checked', 'checked');
+            complete = false;
         }
+        $.get("complete-todo/" + id + "?complete=" + complete, function (data) {
+            if (complete) {
+                $self.attr('checked', 'checked');
+            }else{
+                $self.removeAttr('checked');
+            } 
 
-        $(this).closest("li").toggleClass('completed');
+            $self .closest("li").toggleClass('completed');          
+        })
     });
 
     todoListItem.on('click', '.remove', function() {
-        $(this).parent().remove();
+        // $(this).parent().remove();
+        // url : todos/id  mehtod : DELETE
+        var id = $(this).closest("li").attr('id');
+        var $self = $(this);
+        $.ajax({
+            url: "todos/" + id,
+            type: "DELETE",
+            success: function (data) {
+                if (data.success) {
+                    $self.parent().remove();
+                }
+            }
+        })
     });
 
 });
